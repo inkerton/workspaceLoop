cb17066c870e0b4950fdb2941fcf44020013f4ea
Janvi <janvichoudhary116@gmail.com>
2024-09-01 19:53:54 +0530

    comment log created

--- src/app/api/comment/route.js
+++ src/app/api/comment/route.js
@@ -1,6 +1,7 @@
 import dbConnect from "@/utils/mongodb";
 import Comment from "@/model/Comment";
 import { NextResponse } from "next/server";
+import ChangeLog from "@/model/ChangeLogs";
 
 export async function POST(request) {
   await dbConnect();
@@ -26,13 +27,20 @@ export async function POST(request) {
       });
 
       await newComment.save();
-      console.log("comment saved -----------");
+
+      const changeLog = new ChangeLog({
+        username: comment.username,
+        action: `New Comment Posted`,
+        timeOfAction: comment.date,
+      });
+
+      await changeLog.save();
+
       return NextResponse.json(
         { message: "Comment Saved!!!" },
         { status: 200 }
       );
     } else {
-      console.log("object found");
       existingComment.comments.push({
         username: comment.username,
         text: comment.text,
@@ -40,7 +48,15 @@ export async function POST(request) {
       });
 
       await existingComment.save();
-      console.log("object updated as new entry pushed");
+
+      const changeLog = new ChangeLog({
+        username: comment.username,
+        action: `New Comment Pushed to an Existing one`,
+        timeOfAction: comment.date,
+      });
+
+      await changeLog.save();
+
       return NextResponse.json({ message: "Comment Updated" }, { status: 201 });
     }
   } catch (error) {
@@ -57,21 +73,25 @@ export async function GET(request) {
   try {
     const url = new URL(request.url);
     const incidentNo = url.searchParams.get("incidentNo");
-    console.log('object', incidentNo);
+    console.log("object", incidentNo);
 
     if (!incidentNo) {
-        return new Response(JSON.stringify({ message: 'Incident number is required' }), { status: 400 });
-      }
+      return new Response(
+        JSON.stringify({ message: "Incident number is required" }),
+        { status: 400 }
+      );
+    }
 
     const comment = await Comment.findOne({ incidentNo });
-    console.log('object comm', comment);
+    console.log("object comm", comment);
 
     if (!comment) {
-        return new Response(JSON.stringify({ message: 'Comments not found' }), { status: 404 });
-      }
+      return new Response(JSON.stringify({ message: "Comments not found" }), {
+        status: 404,
+      });
+    }
 
     return NextResponse.json(comment, { status: 200 });
-
   } catch (error) {
     console.log("error", error);
     return NextResponse.json(
